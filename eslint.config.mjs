591ec9acabf30ab43f123c2d358abb5e3b51f55f
valtervalik <valtervaralik@gmail.com>
2025-04-20 17:17:02 -0400

    chore: :heavy_plus_sign: Update ESLint configuration and dependencies
    - Refactored ESLint configuration to use single quotes and added @tanstack/eslint-plugin-query. - Updated package.json to include new dependencies: @hookform/resolvers, @radix-ui/react-slot, @tanstack/react-query, @tanstack/react-query-devtools, axios, axios-auth-refresh, class-variance-authority, clsx, lucide-react, notistack, react-hook-form, tailwind-merge, tw-animate-css, and zod. - Updated devDependencies in package.json to include @eslint/eslintrc, @tailwindcss/postcss, and @tanstack/eslint-plugin-query. - Updated pnpm-lock.yaml to reflect new dependencies and their versions. - Updated postcss.config.mjs to use single quotes. - Added components.json with updated structure and aliases. - Created button.tsx component with button variants using class-variance-authority. - Created utils.ts for utility functions using clsx and tailwind-merge.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...pluginQuery.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
