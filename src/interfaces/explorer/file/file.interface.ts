3c27b44a4e9512c0750c0b1ff487510725b1fd89
valtervalik <valtervaralik@gmail.com>
2025-06-14 17:59:48 -0400

    feat: add FileStatusEnum and integrate status in file handling components

--- src/interfaces/explorer/file/file.interface.ts
+++ src/interfaces/explorer/file/file.interface.ts
@@ -1,7 +1,10 @@
+import { FileStatusEnum } from '@/lib/constants/files.enum';
+
 export interface File {
   _id: string;
   filename: string;
   ref: string;
   size: number;
   compressedSize: number;
+  status: FileStatusEnum;
 }
---
+++ src/lib/constants/files.enum.ts
@@ -0,0 +1,4 @@
+export enum FileStatusEnum {
+  RAW = 'raw',
+  COMPRESSED = 'compressed',
+}
